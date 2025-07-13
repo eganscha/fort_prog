6c7c987b7cf1d59b46b0fa358722fafe3fdd13da
Eugen Ganscha <eganscha@gmail.com>
2025-07-13 15:43:38 +0200

    Added comments to aufgabe2.

--- aufgabe2/src/main/java/org/example/Main.java
+++ aufgabe2/src/main/java/org/example/Main.java
@@ -27,6 +27,8 @@ public class Main {
 
         // Ausgabe des Wertes
         System.out.println("Ablauf1:\nval: " + counter.val);
+        // Ergebnis: Praktisch immer 4_000_000, weil wir zuerst 2_000_000 count aufrufen, bevor wir überhaupt mit der Parallelisierung beginnen.
+        // (einzige potenzielle Fehlerquelle wäre, wenn 2s Wartezeit nicht reichen würden)
     }
 
     public static void ablauf2() {
@@ -48,5 +50,7 @@ public class Main {
 
         // Ausgabe des Wertes
         System.out.println("Ablauf2:\nval: " + counter.val);
+        // Ergebnis: Immer deutlich <4_000_000, weil wir systematisch lost updates produzieren, weil beide Threads parallel schreiben.
+        // Mögliche Lösung: Atomic Int oder Methodenaufruf mithilfe von synchronized keyword.
     }
 }
